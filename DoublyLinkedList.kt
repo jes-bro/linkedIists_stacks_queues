23cd970b31aa66400db0162754041081e5e70880
Jessica Brown <91238623+jes-bro@users.noreply.github.com>
2024-02-03 22:38:33 -0500

    Add more in-line comments

--- DoublyLinkedList.kt
+++ DoublyLinkedList.kt
@@ -62,7 +62,9 @@ class DoublyLinkedList<T>: DoublyLinkedListUniversal<T> {
     internal var head: LinkedListNode<T>?=null
     // The tail of the doubly linked list
     internal var tail: LinkedListNode<T>?=null
+    // The linked list node class
     internal class LinkedListNode<T>(val data: T, var next: LinkedListNode<T>?, var prev: LinkedListNode<T>?)
+    
     /**
      * Adds the element [data] to the front of the linked list.
      * @param data The information being stored in the new head
@@ -77,6 +79,7 @@ class DoublyLinkedList<T>: DoublyLinkedListUniversal<T> {
         }
         this.head = new_node
     }
+    
     /**
      * Adds the element [data] to the back of the linked list.
      * @param data The information being stored in the new tail
@@ -91,6 +94,7 @@ class DoublyLinkedList<T>: DoublyLinkedListUniversal<T> {
         }
         this.tail = new_node
     }
+    
     /**
      * Removes an element from the front of the list. If the list is empty, it is unchanged.
      * @return the value at the front of the list or nil if none exists.
